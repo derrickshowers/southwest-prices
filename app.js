bc9f9384b632eb69b9edbf24d2b5b0a9a4c8a327
Derrick Showers <dshowers@linkedin.com>
2015-02-22 16:31:59 -0800

    added timer to check prices over and over and over and...

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 var Prices = require('./prices');
 
-Prices.setData({
-  origin: 'BWI',
-  destination: 'OAK',
-  outboundDate: '05/16/2015',
-  returnDate: '05/23/2015'
-})
+function getNewPrices() {
+  return Prices.getPrices();
+}
 
-Prices.getPrices().then(function(matches) {
+function processPrices(matches) {
   console.log(matches);
-});
+}
+
+function timer(delay) {
+  setTimeout(function() {
+    getNewPrices().then(function(matches) {
+      // when promise is resolved...
+
+      // process the results
+      processPrices(matches);
+
+      // reset the timer
+      timer(5000);
+    });
+  }, delay)
+}
+
+function init() {
+
+  // set things up
+  Prices.setData({
+    origin: 'BWI',
+    destination: 'OAK',
+    outboundDate: '05/16/2015',
+    returnDate: '05/23/2015'
+  });
+
+  // start the timer (no initial delay)
+  timer(0);
+}
+
+init();
